3c32d90c16af7332bd6b6b449bde5e0a5cc7864d
NazarGorynych <nazarii.horchytsia@oa.edu.ua>
2021-09-17 20:25:13 +0300

    added forms.py + model User

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+# from django.contrib.auth.models import User
+# Create your models here.
+
+class User(models.Model):
+
+    nickname = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
--- blog_website/settings.py
+++ blog_website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+3'
 
 USE_I18N = True
 
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
