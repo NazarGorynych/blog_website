86cf6884e5aad66a3a17cc02de89b0704cf756d3
NazarGorynych <nazarii.horchytsia@oa.edu.ua>
2021-09-23 19:43:20 +0300

    added Post model

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,16 @@ from django.db import models
 
 class User(models.Model):
 
-    nickname = models.CharField(max_length=200, unique=True)
+    nickname = models.CharField(max_length=30, unique=True)
     email = models.EmailField()
     password = models.CharField(max_length=50)
+
+
+class Post(models.Model):
+
+    published_date = models.DateField()
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # slug = models.SlugField(unique=True)
+    # text = models.CharField(max_length=300)
+    # image = models.ImageField(upload_to='images')
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def homepage(request):
+    return render(request, 'blog/homepage.html')
